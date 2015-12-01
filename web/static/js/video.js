733b07c1c9f44387d4e6055c78618ad4ec964021
Dave Hackett <tallguy.hackett@gmail.com>
2015-11-30 21:46:13 -0700

    Add basic channel ping message to verify channel is working

--- web/static/js/video.js
+++ web/static/js/video.js
@@ -12,6 +12,7 @@ let Video = {
 
 		socket.connect()
 		let vidChannel = socket.channel("videos:" + videoId)
+		vidChannel.on("ping", ({count}) => console.log("PING", count))
 
 		vidChannel.join()
 			.receive("ok", resp => console.log("joined the video channel", resp) )
