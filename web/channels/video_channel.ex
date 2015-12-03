994bef2031f2622b9fd5395c1036df409d48f088
Dave Hackett <tallguy.hackett@gmail.com>
2015-12-02 23:34:24 -0700

    Persist annotations and display them

--- web/channels/video_channel.ex
+++ web/channels/video_channel.ex
@@ -1,17 +1,39 @@
 defmodule Rumbl.VideoChannel do
   use Rumbl.Web, :channel
+  alias Rumbl.AnnotationView
 
   def join("videos:" <> video_id, _params, socket) do
-    {:ok, assign(socket, :video_id, video_id)}
+    video = Repo.get!(Rumbl.Video, video_id)
+    annotations = Repo.all(
+      from a in assoc(video, :annotations),
+      order_by: [desc: a.at],
+      limit: 200,
+      preload: [:user]
+    )
+
+    resp = %{annotations: Phoenix.View.render_many(annotations, AnnotationView, "annotation.json")}
+
+    {:ok, resp, assign(socket, :video_id, video.id)}
   end
 
   def handle_in("new_annotation", params, socket) do
-    broadcast! socket, "new_annotation", %{
-      user: %{username: "anon"},
-      body: params["body"],
-      at: params["at"]
-    }
+    user = socket.assigns.current_user
 
-    {:reply, :ok, socket}
+    changeset =
+      user
+      |> build(:annotations, video_id: socket.assigns.video_id)
+      |> Rumbl.Annotation.changeset(params)
+
+    case Repo.insert(changeset) do
+      {:ok, annotation} ->
+        broadcast! socket, "new_annotation", %{
+          user: Rumbl.UserView.render("user.json", %{user: user}),
+          body: annotation.body,
+          at: annotation.at
+        }
+        {:reply, :ok, socket}
+      {:error, changeset} ->
+        {:reply, {:error, %{errors: changeset}}, socket}
+    end
   end
 end
